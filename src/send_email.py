9983dca809f24ebab72d9f600e6892570abb5f9f
sachin devkar <sachin.devkar@sts.in>
2022-08-01 18:30:50 +0530

    added changes

--- src/send_email.py
+++ src/send_email.py
@@ -282,17 +282,26 @@ COLLECTOR = VulnerabilityCollector()
 
 
 def main():
-    
-    options = {"owner": "", "authtoken": ""}
-    owner = os.environ.get("GITHUB_OWNER")
-    authtoken = os.environ.get("GITHUB_AUTHTOKEN")
+    parser = argparse.ArgumentParser(description="Export GitHub vulnerability alerts")
+    parser.add_argument("--owner", help="GitHub owner name")
+    parser.add_argument("--authtoken", help="GitHub API token")
+    parser.add_argument(
+        "--short",
+        action="store_true",
+        help="Short form output, otherwise defaults to long form detailed output",
+    )
+    options = parser.parse_args()
+    if not options.owner:
+        options.owner = os.environ.get("GITHUB_OWNER")
+    if not options.authtoken:
+        options.authtoken = os.environ.get("GITHUB_AUTHTOKEN")
 
-    #if not all([options.owner, options.authtoken]):
-    #    parser.print_help()
-    #    raise SystemExit(1)
+    if not all([options.owner, options.authtoken]):
+        parser.print_help()
+        raise SystemExit(1)
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
-    #print("----------------------------------------", options.authtoken)
-    COLLECTOR.configure(owner, authtoken)
+
+    COLLECTOR.configure(options.owner, options.authtoken)
     COLLECTOR.collect()
     if options.short:
         print_report(COLLECTOR.repos)
